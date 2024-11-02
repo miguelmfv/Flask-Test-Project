b3a28501fcd256185db7d37e96935de75df35e68
Miguel F. Vivan <100147829+miguelmfv@users.noreply.github.com>
2024-11-01 23:14:47 -0300

    Fnished project

--- app.py
+++ app.py
@@ -1,26 +1,28 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_migrate import Migrate
-import os
-from dotenv import load_dotenv
+from sqlalchemy.testing.pickleable import Order
+
 from extensions import db
+import datetime
 
 app = Flask(__name__)
-load_dotenv()
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://dbweb2:dbweb2@localhost:5432/BancoDevWeb2'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 migrate = Migrate(app, db)
 
-from models import Product
-from models import Clients
-
+from models import Product, Orders, Clients
 
 @app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/products')
+def indexProducts():
     products = Product.query.all()
-    return render_template('index.html', products=products)
+    return render_template('indexProducts.html', products=products)
 
 
 @app.route('/clients')
@@ -29,7 +31,13 @@ def indexClients():
     return render_template('indexClients.html', clients=clients)
 
 
-@app.route('/create', methods=['GET', 'POST'])
+@app.route('/orders')
+def indexOrders():
+    orders = Orders.query.all()
+    return render_template('indexOrders.html', orders=orders)
+
+
+@app.route('/createProducts', methods=['GET', 'POST'])
 def create():
     if request.method == 'POST':
         name = request.form['name']
@@ -38,8 +46,8 @@ def create():
         product = Product(name=name, descricao=description, preco=price)
         db.session.add(product)
         db.session.commit()
-        return redirect(url_for('index'))
-    return render_template('create.html')
+        return redirect(url_for('indexProducts'))
+    return render_template('createProducts.html')
 
 
 @app.route('/createClient', methods=['GET', 'POST'])
@@ -48,25 +56,36 @@ def createClient():
         name = request.form['name']
         email = request.form['email']
         born = request.form['born']
-        bought = request.form['bought']
-        client = Clients(name=name, email=email, dataNasc=born, bought=bought)
+        bought = request.form.get('bought') == 'true'
+        client = Clients(name=name, email=email, bougth=bought, dataNasc=born)
         db.session.add(client)
         db.session.commit()
         return redirect(url_for('indexClients'))
     return render_template('createClients.html')
 
 
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
-def update(id):
+@app.route('/createOrders', methods=['GET', 'POST'])
+def createOrders():
+    if request.method == 'POST':
+        nameClient = request.form['nameClient']
+        nameProduct = request.form['nameProduct']
+        numItems = request.form['numItems']
+        order = Orders(nameClient = nameClient, nameProduct = nameProduct, numItems = numItems)
+        db.session.add(order)
+        db.session.commit()
+        return redirect(url_for('indexOrders'))
+    return render_template('createOrders.html')
 
+@app.route('/updateProducts/<int:id>', methods=['GET', 'POST'])
+def updateProducts(id):
     product = Product.query.get_or_404(id)
     if request.method == 'POST':
         product.name = request.form['name']
         product.descricao = request.form['description']
         product.preco = request.form['price']
         db.session.commit()
-        return redirect(url_for('index'))
-    return render_template('update.html', product=product)
+        return redirect(url_for('indexProducts'))
+    return render_template('updateProducts.html', product=product)
 
 
 @app.route('/updateClient/<int:id>', methods=['GET', 'POST'])
@@ -76,10 +95,21 @@ def updateClient(id):
         client.name = request.form['name']
         client.email = request.form['email']
         client.dataNasc = request.form['born']
-        client.bought = request.form['bought']
+        client.bougth = request.form.get('bougth') == 'true'
         db.session.commit()
         return redirect(url_for('indexClients'))
     return render_template('updateClients.html', client=client)
+
+@app.route('/updateOrder/<int:id>', methods=['GET', 'POST'])
+def updateOrder(id):
+    order = Orders.query.get_or_404(id)
+    if request.method == 'POST':
+        order.nameClient = request.form['nameClient']
+        order.nameProduct = request.form['nameProduct']
+        order.numItems = request.form['numItems']
+        db.session.commit()
+        return redirect(url_for('indexOrders'))
+    return render_template('updateOrders.html', order=order)
 
 
 @app.route('/delete/<int:id>')
@@ -88,7 +118,7 @@ def delete(id):
     db.session.delete(product)
     db.session.commit()
 
-    return redirect(url_for('index'))
+    return redirect(url_for('indexProducts'))
 
 
 @app.route('/deleteClient/<int:id>')
@@ -98,6 +128,14 @@ def deleteClient(id):
     db.session.commit()
 
     return redirect(url_for('indexClients'))
+
+@app.route('/deleteOrder/<int:id>')
+def deleteOrder(id):
+    order = Orders.query.get_or_404(id)
+    db.session.delete(order)
+    db.session.commit()
+
+    return redirect(url_for('indexOrders'))
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,3 +1,5 @@
+from symtable import Class
+
 from extensions import db
 
 class Product(db.Model):
@@ -18,9 +20,20 @@ class Clients(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(200), nullable=False)
-    dataNasc = db.Column(db.Float, nullable=False)
     bougth = db.Column(db.Boolean, nullable=False)
-
+    dataNasc = db.Column(db.Date, nullable=False)
 
     def __repr__(self):
         return f'<Clients {self.name}>'
+
+
+class Orders(db.Model):
+    __tablename__ = 'orders'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nameClient = db.Column(db.String(100), nullable=False)
+    nameProduct = db.Column(db.String(100), nullable=False)
+    numItems = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f'<Orders {self.id}>'
